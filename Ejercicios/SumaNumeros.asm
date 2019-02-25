62174c02f46fd5a9b57109859cfc45b91c233a80
CarlosGavidia <47639466+CarlosGavidia@users.noreply.github.com>
2019-02-25 14:15:02 +0100

    Create SumaNumeros.asm

---
+++ Ejercicios/SumaNumeros.asm
@@ -0,0 +1,28 @@
+.global start
+.data
+C: 				.word 0X00
+A:    			.word 0x0A
+B:   			.word 0x03
+.bss
+
+ .text
+ start:
+
+			LDR R2 , = A
+			LDR R3 , [R2]
+			LDR R4 , = B
+			LDR R5 , = [R4]
+
+	while: CMP R3, R5
+			BLT fin_while
+			SUB R3, R3, R5
+			ADD R1, R1, #1
+
+			B while
+
+	fin_while:
+	LDR R0, =C
+	STR R0,[R1]
+	B .
+
+	.end
